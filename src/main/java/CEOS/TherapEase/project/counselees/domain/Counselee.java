0a000d11e9377f8c4f5fa806d1899c2d0c254bc7
Sojeong Lee <sj27419@gmail.com>
2023-07-07 00:28:44 +0900

    add Counselee.java

--- src/main/java/CEOS/TherapEase/project/counselees/domain/Counselee.java
+++ src/main/java/CEOS/TherapEase/project/counselees/domain/Counselee.java
@@ -1,4 +1,43 @@
 package CEOS.TherapEase.project.counselees.domain;
 
-public class Counselee {
+import CEOS.TherapEase.project.accounts.domain.Account;
+import CEOS.TherapEase.project.global.entity.BaseTimeEntity;
+import com.fasterxml.jackson.databind.ser.Serializers;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Counselee extends BaseTimeEntity {
+    //키값
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "counseleeName", updatable = false)
+    private Long counseleeName;
+
+    // 상담 시작 날짜
+    @Column
+    private String start; // yyyy-mm-dd 형식으로 적으시오.
+
+    // 진행 상황
+    @Column
+    private Integer progress; // 진행 = 1, 그 외에는 0
+
+    // 내담자를 맡은 상담사
+    @OneToMany
+    @JoinColumn(name="counselorId")
+    private Account counselor;
+
+    @Builder
+    public Counselee (String start,String progress){
+        this.start = start;
+        this.progress = Integer.valueOf(progress);
+        this.counselor = counselor;
+    }
+
 }
