ad9cc0c30b7bf02e6d3f391306c7d87b1d5d19b8
Hwang-Jaeryeong <jryeong67@naver.com>
2023-07-09 01:11:02 +0900

    [add] accounts

---
+++ src/main/java/CEOS/TherapEase/project/accounts/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package CEOS.TherapEase.project.accounts.repository;
+import CEOS.TherapEase.project.accounts.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Boolean existsByEmail(String email);
+    Optional<Account> findByEmail(String email); // 이메일로 계정 조회
+}
